7b14c7e5d503862b9d18bedab6f6601b352af236
abdelfetah18 <nstart0319@gmail.com>
2023-11-01 15:25:33 +0100

    Remove provide/inject

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,11 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
-import RestInfo from './libs/RestInfo';
-import axios from 'axios';
 import { createRouter, createWebHashHistory } from 'vue-router';
 import { HomeVue } from './screens/home';
 
 
 const app = createApp(App);
-
-const restInfo = new RestInfo(axios);
-
-app.provide('restInfo', restInfo);
 
 const routes = [
     { path: '/', component: HomeVue },
--- frontend/src/screens/home/Home.component.js
+++ frontend/src/screens/home/Home.component.js
@@ -1,11 +1,14 @@
-import { defineComponent, inject, onMounted, ref } from "vue";
+import RestInfo from "@/libs/RestInfo";
+import axios from "axios";
+import { defineComponent, onMounted, ref } from "vue";
 
 export default defineComponent({
     name: 'HomeVue',
     props: {},
     setup() {
         let data = ref(null);
-        const restInfo = inject('restInfo');
+
+        const restInfo = new RestInfo(axios);
 
         onMounted(async () => {
             data.value = await restInfo.getInfo();
