5b4229ede069fee849f5dfb911d5ed8263f9aad1
abdelfetah18 <nstart0319@gmail.com>
2023-11-13 19:47:45 +0100

    update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            UserSeeder::class,
+            PricesTableSeeder::class,
+            CategoriesTableSeeder::class,
+            ServiceTableSeeder::class,
+        ]);
+    
     }
 }
