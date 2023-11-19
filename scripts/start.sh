637bd813a6d17ec46cd4939b33f1ae38a1a8ae2b
abdelfetah18 <nstart0319@gmail.com>
2023-11-19 17:51:13 +0100

    update CI/CD pipeline for the production

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,6 +13,9 @@ cd ../
 echo "# JWT Secret Generate..."
 php artisan jwt:secret
 
+echo "# Link storage..."
+php artisan storage:link
+
 echo "# Start the PHP application in the background"
 php artisan serve --host=0.0.0.0 --port=8000 &
 
